0c4cb5e3680fe49fdf15322a9301fc94ff818e66
seokbong <ssb7779@naver.com>
2025-04-04 11:43:30 +0900

    상세보기 옵션코드 제거

--- src/main/java/com/cjoa/wms/view/ResultMainView.java
+++ src/main/java/com/cjoa/wms/view/ResultMainView.java
@@ -3,6 +3,7 @@ package com.cjoa.wms.view;
 import com.cjoa.wms.dto.ProductDto;
 import com.cjoa.wms.dto.ProductOptionDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResultMainView {
@@ -25,20 +26,21 @@ public class ResultMainView {
     }
 
 
-    public static void displayProductOptionList(List<ProductDto> list){
-
-        if(list.isEmpty()){
+    public static void displayProductOptionList(ProductDto product){
+        if(product == null){
             System.out.println("없는 메뉴 번호입니다☹");
         }else {
-            for (ProductDto product : list)  {
-                System.out.printf("%d, %s, %s, %s, %d원\n",
-                        product.getProductOptionDto().getProdOptionCode(),
-                        product.getProdName(),
-                        product.getProductOptionDto().getProdSize(),
-                        product.getProductOptionDto().getProdColor(),
-                        product.getProdPrice());
+            System.out.print(product.getProdName() + "\t");
+            System.out.print(product.getProdPrice());
+            System.out.println("");
 
+            List<ProductOptionDto> list = product.getProductOptionList();
+            for (ProductOptionDto option : list) {
+                System.out.print(option.getProdColor() + "\t");
+                System.out.print(option.getProdSize() + "\t");
+                System.out.println("");
             }
+
         }
     }
 
